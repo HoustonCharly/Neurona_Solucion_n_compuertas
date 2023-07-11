2775b1ca6aec6307744df0c94e190074601fdd72
HoustonCharly <jriverar1801@alumno.ipn.mx>
2023-07-11 15:05:07 -0600

    First name

---
+++ Practica3/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Practica3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tBEntradas = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Gridpesos = new System.Windows.Forms.DataGridView();
+            this.Gridtabla = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Label = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tBumbral = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.firmaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.Gridpesos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gridtabla)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No. de Entradas";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tBEntradas
+            // 
+            this.tBEntradas.Font = new System.Drawing.Font("Bahnschrift SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBEntradas.Location = new System.Drawing.Point(144, 64);
+            this.tBEntradas.Name = "tBEntradas";
+            this.tBEntradas.Size = new System.Drawing.Size(80, 27);
+            this.tBEntradas.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bahnschrift SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(59, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generar Tabla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Gridpesos
+            // 
+            this.Gridpesos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Gridpesos.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.Gridpesos.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.Gridpesos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Gridpesos.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Gridpesos.Location = new System.Drawing.Point(261, 69);
+            this.Gridpesos.Name = "Gridpesos";
+            this.Gridpesos.Size = new System.Drawing.Size(161, 150);
+            this.Gridpesos.TabIndex = 3;
+            // 
+            // Gridtabla
+            // 
+            this.Gridtabla.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Gridtabla.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.Gridtabla.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.Gridtabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Gridtabla.Location = new System.Drawing.Point(26, 241);
+            this.Gridtabla.Name = "Gridtabla";
+            this.Gridtabla.Size = new System.Drawing.Size(575, 254);
+            this.Gridtabla.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Bahnschrift SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(486, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Neurona";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Bahnschrift SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(511, 191);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 28);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Limpiar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Label
+            // 
+            this.Label.Font = new System.Drawing.Font("Bahnschrift SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label.Location = new System.Drawing.Point(424, 62);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(77, 23);
+            this.Label.TabIndex = 7;
+            this.Label.Text = "Umbral";
+            this.Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(257, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 22);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Pesos";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Bahnschrift SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 27);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tabla de Verdad";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tBumbral
+            // 
+            this.tBumbral.Font = new System.Drawing.Font("Bahnschrift SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBumbral.Location = new System.Drawing.Point(511, 64);
+            this.tBumbral.Name = "tBumbral";
+            this.tBumbral.Size = new System.Drawing.Size(90, 27);
+            this.tBumbral.TabIndex = 10;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.menuStrip1.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.firmaToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(626, 26);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // firmaToolStripMenuItem
+            // 
+            this.firmaToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.firmaToolStripMenuItem.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firmaToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.firmaToolStripMenuItem.Name = "firmaToolStripMenuItem";
+            this.firmaToolStripMenuItem.Size = new System.Drawing.Size(61, 22);
+            this.firmaToolStripMenuItem.Text = "Firma";
+            this.firmaToolStripMenuItem.Click += new System.EventHandler(this.firmaToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.salirToolStripMenuItem.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salirToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(51, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(626, 521);
+            this.Controls.Add(this.tBumbral);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Gridtabla);
+            this.Controls.Add(this.Gridpesos);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tBEntradas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Neurona Tabla de Verdad";
+            ((System.ComponentModel.ISupportInitialize)(this.Gridpesos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gridtabla)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tBEntradas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView Gridpesos;
+        private System.Windows.Forms.DataGridView Gridtabla;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tBumbral;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem firmaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+    }
+}
+
---
+++ Practica3/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Practica3
+{
+    public partial class Form1 : Form
+    {
+        int entrada;
+        double[,] datos;
+        double[] pesos, neurona;
+        double tamano, umbral;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            entrada = int.Parse(tBEntradas.Text);//lee el dato que entra el en textbox
+            tamano = Math.Pow(2, entrada);//Se eleva 2^n para obtenr el tamaño de la tabla
+            datos = new double[(int)tamano, entrada];//Vector usando tamaño y entradas
+
+            //Se comienza a generar la tabla de acuerdo al numero de bits de entrada
+            Gridtabla.RowCount = (int)tamano;//genera las filas para los datos
+            for (int i = 0; i < entrada; i++)
+            {
+                Gridtabla.Columns.Add("valX", "X"+(i));//el primer valor es X0 hasta X+1
+            }
+            Gridtabla.Columns[entrada].HeaderText = "Y";
+
+            int contador = 0;
+            int aux1 = 0;
+            if (entrada > 0)
+            {
+                int aux2 = (int)tamano / 2;
+                for (int j = 0; j < entrada; j++)
+                {
+                    for (int i = 0; i < tamano; i++)
+                    {
+                        if (aux1 == 0)
+                        {
+                            datos[i, j] = 0;
+                            Gridtabla.Rows[i].Cells[j].Value = 0;
+                            contador++;
+
+                        }
+                        else if (aux1 == 1)
+                        {
+                            datos[i, j] = 1;
+                            Gridtabla.Rows[i].Cells[j].Value = 1;
+                            contador--;
+                        }
+                        if (contador == aux2)
+                        {
+                            aux1 = 1;
+                        }
+                        if (contador == 0)
+                        {
+                            aux1 = 0;
+                        }
+                    }
+                    aux2 = aux2 / 2;
+                }
+            }
+            //limpia por si se desea cambiar el valor de los pesos
+            Gridpesos.Columns.Clear();
+            Gridpesos.Rows.Clear();
+            //para el vector de la Tablita de pesos
+            pesos = new double[entrada];
+            Gridpesos.Columns.Add("pesos", "Pesos");
+            for (int i = 0; i < entrada; i++)
+            {
+                Gridpesos.Rows.Add();
+            }
+        }
+
+        private void firmaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("LOS INCREIBLES:\n" +
+                "\tRivera Rivera Juan Carlos\n" );
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            neurona = new double[(int)tamano];
+            umbral = double.Parse(tBumbral.Text);//Lee el valor del textbox umbral
+            Gridtabla.Columns[entrada].HeaderText = "Salida";
+            try
+            {
+                //Genera un Vector que Guarda los datos de los pesos insertados
+                for (int p = 0; p < entrada; p++)
+                {
+                    pesos[p] = double.Parse(Gridpesos.Rows[p].Cells[0].Value.ToString());
+                }
+                Gridtabla.ColumnCount = entrada+1;//Se agrega la columna de salida
+                //Se hace la operacion evaluando las columnas de la tabla por el vector de pesos
+                for (int n = 0; n < tamano; n++)
+                {
+                    for (int k = 0; k<entrada; k++)
+                    {
+                        neurona[n] += datos[n, k]*pesos[k];
+                        
+                        if (neurona[n]>umbral)
+                        {
+                            Gridtabla.Rows[n].Cells[entrada].Value = 1;
+                            //imprime en el grid si es este el caso si no
+                        }
+                        else
+                        {
+                            Gridtabla.Rows[n].Cells[entrada].Value = 0;
+                            //imrpime este otro en el grid
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en la entrada de datos...!");
+            }
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Gridpesos.Columns.Clear();
+            Gridtabla.Columns.Clear();
+            tBEntradas.Clear();
+            tBumbral.Clear();
+
+        }
+
+    }
+}
